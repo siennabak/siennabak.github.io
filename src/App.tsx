f130d303e031027aca1fe18ba55ffcdbb55e87dd
siennabak <sieun.park.91@gmail.com>
2023-04-10 22:17:12 +0900

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import { useState }  from 'react';
 import GlobalStyles from './components/GlobalStyles';
 import Router from './Router';
 import { HelmetProvider } from 'react-helmet-async';
